2d10963a17c152c29f535099295fc9ea27d1ca6c
Gábor Szabó <gabe.szabo@outlook.com>
2018-08-08 11:07:57 +0200

    Documentation finished with FilterClause and QueryExecutorBase

--- src/FilterMutator/FilterMutator.Core/FilterClause.cs
+++ src/FilterMutator/FilterMutator.Core/FilterClause.cs
@@ -1,14 +1,43 @@
-﻿using MutatorFX.Coding;
-using System;
-using System.Collections;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 
 namespace MutatorFX.FilterMutator
 {
+    /// <summary>
+    /// Represents the default implementation of a clause used for filtering. Provides a way to select 
+    /// a clause from a filter object, execute the clause on a given dataset, or fallback to a default
+    /// filtering mechanism when the clause is considered disabled.
+    /// </summary>
+    /// <typeparam name="TSource">The type of the source objects in the dataset to be filtered.</typeparam>
+    /// <typeparam name="TFilter">The filter type to select the clause from to apply to the dataset.</typeparam>
+    /// <typeparam name="TClause">The type of the selected clause.</typeparam>
     public class FilterClause<TSource, TFilter, TClause> : IFilterClause<TSource, TFilter>
     {
+        /// <summary>
+        /// Create a new clause used for filtering.
+        /// </summary>
+        /// <param name="filterClauseSelector">
+        /// A selector that can be used to aquire the clause value from a given filter. As 
+        /// it is an Expression, it can be used to rationalize about a collection of clauses.
+        /// </param>
+        /// <param name="filterPredicate">
+        /// The predicate factory to use when building the filtering expressions. Can be a lambda,
+        /// that gets the selected clause value from the filter as parameter, that returns another lamdbda
+        /// that uses closure on the outer clause parameter to apply dynamically parametered filtering.
+        /// <para/>Example:
+        /// <code lang="csharp">
+        /// (clause: Clause) => (source: TSource) => source.Name.Contains(clause); 
+        /// <para/> // shorter version: <para/> c => s => s.Name.Contains(c);
+        /// </code>
+        /// </param>
+        /// <param name="onDisabledFilterPredicate">A predicate that gets used instead of 
+        /// <paramref name="filterPredicate"/> when the clause is considered disabled.</param>
+        /// <param name="isClauseEnabled">A predicate that indicates whether the clause is to be
+        /// considered enabled.</param>
+        /// <param name="options">Additional untyped options. Can be used for extensibility to add 
+        /// state or configuration to the clause via extensions.</param>
         public FilterClause(
             Expression<Func<TFilter, TClause>> filterClauseSelector,
             Func<TClause, Expression<Func<TSource, bool>>> filterPredicate = null,
@@ -24,24 +53,70 @@ namespace MutatorFX.FilterMutator
             Options = options;
         }
 
-        public Func<TFilter, bool> IsClauseEnabledFunc { get; }
+        private Func<TFilter, bool> IsClauseEnabledFunc { get; }
+        private Func<TFilter, TClause> FilterClauseSelectorFunc { get; }
+        private Func<TClause, Expression<Func<TSource, bool>>> FilterPredicate { get; }
+        private Expression<Func<TSource, bool>> OnDisabledFilterPredicate { get; }
+        
+        /// <summary>
+        /// The options passed to this clause instance used to apply additional logic 
+        /// based on state via extensions.
+        /// </summary>
         public IReadOnlyDictionary<string, object> Options { get; }
-        public Func<TFilter, TClause> FilterClauseSelectorFunc { get; }
-        public Func<TClause, Expression<Func<TSource, bool>>> FilterPredicate { get; }
-        public Expression<Func<TSource, bool>> OnDisabledFilterPredicate { get; }
+
+        /// <summary>
+        /// The expression that represents the clause selector. Can be used to rationalize
+        /// about this clause in regards to other clauses (i.e. to check whether all filter
+        /// properties are matched in a collection of clauses).
+        /// </summary>
         public Expression ClauseExpression { get; }
 
+        /// <summary>
+        /// Gets the clause object's value by applying the clause selector to the filter.
+        /// </summary>
+        /// <param name="filter">The filter object to apply the selector to.</param>
+        /// <returns>The clause value in the given object.</returns>
         object IFilterClause<TSource, TFilter>.GetClause(TFilter filter) => GetClause(filter);
 
+        /// <summary>
+        /// Gets the clause object's value by applying the clause selector to the filter.
+        /// </summary>
+        /// <param name="filter">The filter object to apply the selector to.</param>
+        /// <returns>The clause value in the given object.</returns>
         public virtual TClause GetClause(TFilter filter) => FilterClauseSelectorFunc(filter);
 
+        /// <summary>
+        /// Get the predicate expression for a given source item by a given filter, which
+        /// indicates whether the given source item should be in the filtered resultset or not.
+        /// </summary>
+        /// <param name="filter">The filter object to get the predicate clause from.</param>
+        /// <returns>The expression that represents a predicate delegate, that can be used to
+        /// filter the source dataset by the clause value. Returns null if the 
+        /// <see cref="FilterPredicate"/> is null.</returns>
         public Expression<Func<TSource, bool>> GetFilterPredicate(TFilter filter) => FilterPredicate?.Invoke(GetClause(filter));
 
+        /// <summary>
+        /// Gets the predicate that should be applied when the filter is considered disabled.
+        /// If no custom predicate was supplied, the default is an always truthy predicate.
+        /// </summary>
+        /// <returns>The predicate to use for filtering, or a truthy predicate if none was supplied.</returns>
         public Expression<Func<TSource, bool>> GetOnDisabledFilterPredicate() => OnDisabledFilterPredicate ?? (s => true);
 
+        /// <summary>
+        /// Determine whether the clause is enabled for the given filter.
+        /// Uses the <typeparamref name="TClause"/> type's default <see cref="EqualityComparer{T}"/> if none was supplied.
+        /// </summary>
+        /// <param name="filter">The filter to use when checking the clause's disabled state.</param>
+        /// <returns>True if the filter is considered enabled.</returns>
         public bool IsClauseEnabled(TFilter filter)
             => IsClauseEnabledFunc?.Invoke(filter) ?? !EqualityComparer<TClause>.Default.Equals(GetClause(filter), default);
 
+        /// <summary>
+        /// Executes a filtering by applying one of provided predicates to filter the given datasource.
+        /// </summary>
+        /// <param name="source">The source to filter by this clause's value.</param>
+        /// <param name="filter">The filter to get the clause value from.</param>
+        /// <returns>The dataset filtered by the predicate described by this clause.</returns>
         public virtual IQueryable<TSource> Execute(IQueryable<TSource> source, TFilter filter)
             => source.Where(IsClauseEnabled(filter) ? GetFilterPredicate(filter) ?? (s => true) : GetOnDisabledFilterPredicate());
     }
--- src/FilterMutator/FilterMutator.Core/QueryExecutorBase.cs
+++ src/FilterMutator/FilterMutator.Core/QueryExecutorBase.cs
@@ -3,9 +3,23 @@ using System.Linq;
 
 namespace MutatorFX.FilterMutator
 {
+    /// <summary>A base implementation that provides a paging mechanism and composes the query to a resultset.</summary>
+    /// <typeparam name="TSource">The type of the elements in the source dataset.</typeparam>
+    /// <typeparam name="TResult">The type of the elements in the result dataset.</typeparam>
+    /// <typeparam name="TFilter">The type of the filter used in filtering the dataset.</typeparam>
+    /// <typeparam name="TSort">The enum type that is used to sort the dataset before paging is applied.</typeparam>
     public abstract class QueryExecutorBase<TSource, TResult, TFilter, TSort> : IQueryExecutor<TFilter, TSort, TResult>, ISourceAccessor<TSource>, IFilterer<TSource, TFilter>, ITransformer<TSource, TResult>, ISorter<TSource, TSort>, IPager<TResult>
         where TSort : Enum
     {
+        /// <summary>
+        /// Executes the query with the given filtering, paging and sorting parameters on the source dataset.
+        /// </summary>
+        /// <param name="filter">The filter object to use when filtering the queryable source dataset.</param>
+        /// <param name="page">The page requested for the query.</param>
+        /// <param name="pageSize">The page size requested for the query.</param>
+        /// <param name="sorting">The sorting used when querying the dataset.</param>
+        /// <param name="sortDescending">Whether to use descending ordering of the results in the dataset.</param>
+        /// <returns></returns>
         public virtual IPagedResult<TResult> ExecuteQuery(TFilter filter, int page, int pageSize, TSort sorting, bool sortDescending)
         {
             if (filter == null)
@@ -15,10 +29,42 @@ namespace MutatorFX.FilterMutator
             return new PagedResult<TResult>(Page(Transform(Sort(results, sorting, sortDescending)), page, pageSize), page, pageSize, results.Count());
         }
 
+        /// <summary>
+        /// The accessor for the queryable datasource.
+        /// </summary>
         public abstract IQueryable<TSource> Source { get; }
+
+        /// <summary>
+        /// Filters the given queryable datasource with the given filter.
+        /// </summary>
+        /// <param name="source">The source to filter.</param>
+        /// <param name="filter">The filter object to use when filtering the source.</param>
+        /// <returns>The filtered datasource queryable.</returns>
         public abstract IQueryable<TSource> Filter(IQueryable<TSource> source, TFilter filter);
+
+        /// <summary>
+        /// Transforms the given <typeparamref name="TSource"/> datasource to a set of <typeparamref name="TResult"/> results.
+        /// </summary>
+        /// <param name="source">The source to transform.</param>
+        /// <returns>The transformed queryable datasource.</returns>
         public abstract IQueryable<TResult> Transform(IQueryable<TSource> source);
+
+        /// <summary>
+        /// Sorts the queryable datasource with the given sorting options.
+        /// </summary>
+        /// <param name="filtered">The dataset to sort.</param>
+        /// <param name="sort">The sorting enum used to parameterize the sorting.</param>
+        /// <param name="sortDescending">Whether to use descending ordering for sorting.</param>
+        /// <returns>The sorted queryable dataset.</returns>
         public abstract IQueryable<TSource> Sort(IQueryable<TSource> filtered, TSort sort, bool sortDescending);
+
+        /// <summary>
+        /// Apply paging to a resultset queryable.
+        /// </summary>
+        /// <param name="results">The results to apply paging to.</param>
+        /// <param name="page">The page requested.</param>
+        /// <param name="pageSize">The page size requested.</param>
+        /// <returns>The elements in the queryable corresponding to the requested page.</returns>
         public abstract IQueryable<TResult> Page(IQueryable<TResult> results, int page, int pageSize);
     }
 }
